4f107ea14ba695f8434a397fccd50be4f694f83a
URK21CO2013 <110015085+URK21CO2013@users.noreply.github.com>
2022-11-04 18:34:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,60 +1,43 @@
 class BinaryTreeNode:
- def __init__(self, data):
- self.data = data
- self.left_child = None
- self.right_child = None
- 
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
 def insert(root, new_value) -> BinaryTreeNode:
- 
- # Write your code here
- if (root == None):
- root = BinaryTreeNode(new_value)
- return root
- else:
- if root.data > new_value:
- if root.left_child is None:
- new_node = BinaryTreeNode(new_value)
- root.left_child = new_node
- else:
- insert(root.left_child,new_value)
-else:
- if root.right_child is None:
- new_node = BinaryTreeNode(new_value)
- root.right_child = new_node
- else:
- insert(root.right_child,new_value)
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
+    # Write your code here
+
+
 def inorder(root) -> None:
- """
- to print in the order
- left mid right
- """
- if root:
- inorder(root.left_child)
- print(root.data, end = " ")
- inorder(root.right_child)
+    # Write your code here
+
+
 def preorder(root) -> None:
- """
- to print in the order
- mid left right
- """
- if root:
- print(root.data, end = " ")
- preorder(root.left_child)
- preorder(root.right_child)
+    # Write your code here
+
+
 def postorder(root) -> None:
- """
- to print in the order
- left right left
- """
- if root:
- postorder(root.left_child)
- postorder(root.right_child)
- print(root.data, end = " ")
+    # Write your code here
+
+
 # Do not change the following code
 input_data = input()
 flag = True
 root = None
 for item in input_data.split(', '):
- if flag is True:
- root = insert(None, int(item))
- flag = False
+    if flag is True:
+        root = insert(None, int(item))
+        flag = False
+    else:
+        insert(root, int(item))
+inorder(root)
+print()
+preorder(root)
+print()
+postorder(root)
